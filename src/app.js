ff8a6da418f06e1cc318bd0f9ca12c32b1bea605
nicola giannotta <nicolagiannotta84@gmail.com>
2023-04-16 18:45:41 +0100

    validatePramsNumber,valif=dateBodyNumbers and requireBodyNumbers function added to the GET and POST numbers API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const {
   sayHello,
@@ -38,9 +39,65 @@ app.get('/strings/first-characters/:string', (req, res) => {
 // });
 const { add, subtract, multiply, divide, remainder } = require('./lib/numbers');
 
-app.get('/numbers/add/:number1/and/:number2', (req, res) => {
-  const a = parseInt(req.params.number1, 10);
-  const b = parseInt(req.params.number2, 10);
-  res.status(200).json({ result: add(a, b) });
+function validateParamsNumbers(req, res, next) {
+  const { num1, num2 } = req.params;
+  if (Number.isNaN(Number(num1)) || Number.isNaN(Number(num2))) {
+    return res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
+  return next();
+}
+function validateBodyNumbers(req, res, next) {
+  const { a, b } = req.body;
+  if (Number.isNaN(Number(a)) || Number.isNaN(Number(b))) {
+    return res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  }
+  return next();
+}
+function requireBodyNumber(req, res, next) {
+  const { a, b } = req.body;
+  if (a === undefined || b === undefined) {
+    return res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
+  }
+  return next();
+}
+function errIfdividedByzero(req, res, next) {
+  const { b } = req.body;
+  if (b === 0) {
+    return res.status(400).json({ error: 'Unable to divide by 0.' });
+  }
+  return next();
+}
+app.get('/numbers/add/:num1/and/:num2', validateParamsNumbers, (req, res) => {
+  const { num1, num2 } = req.params;
+  res.status(200).json({ result: add(+num1, +num2) });
 });
+app.get('/numbers/subtract/:num1/from/:num2', validateParamsNumbers, (req, res) => {
+  const { num1, num2 } = req.params;
+  res.status(200).json({ result: subtract(+num2, +num1) });
+});
+app.post('/numbers/multiply', requireBodyNumber, validateBodyNumbers, (req, res) => {
+  const { a, b } = req.body;
+  res.status(200).json({ result: multiply(a, b) });
+});
+app.post(
+  '/numbers/divide',
+  requireBodyNumber,
+  validateBodyNumbers,
+  errIfdividedByzero,
+  (req, res) => {
+    const { a, b } = req.body;
+    res.status(200).json({ result: divide(a, b) });
+  },
+);
+app.post(
+  '/numbers/remainder',
+  requireBodyNumber,
+  validateBodyNumbers,
+  errIfdividedByzero,
+  (req, res) => {
+    const { a, b } = req.body;
+    res.status(200).json({ result: remainder(a, b) });
+  },
+);
+
 module.exports = app;
